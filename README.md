7e7e8526b9ab1a1fb51fd7434e8929089fb483d6
unknown <alvaro3224@gmail.com>
2019-10-31 18:31:05 -0500

    Primer Commit - Rest Server

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+
+# Socket-Server
+
+
+
+Reconstruir módulos de Node
+```
+npm install
+``` 
+
+generar el DIST
+```
+tsc -w
+``` 
+
+Levantar el servidor
+```
+nodemon dist/
+``` 
---
+++ index.ts
@@ -0,0 +1,23 @@
+import Server from "./classes/server";
+import { SERVER_PORT } from "./global/enviroment";
+import router from "./routes/router";
+
+import bodyparser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+//Body parser
+server.app.use(bodyparser.urlencoded({extended:true}));
+server.app.use(bodyparser.json());
+
+//Cors
+server.app.use(cors({origin:true, credentials:true}));
+
+
+//rutas de servicios
+server.app.use('/', router)
+
+server.start(() => {
+    console.log(`Servidor corriendo ${SERVER_PORT}`);
+});
